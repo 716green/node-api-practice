5ec8c245e78c920cb96bdbdbcdc91cadade0924b
716green <bob@bbass.co>
2020-02-17 21:14:57 -0500

    Fixed Everything

--- lesson-1/activity-a/index.js
+++ lesson-1/activity-a/index.js
@@ -2,19 +2,18 @@
 
 // define variable named maths and assign it to the export module from maths.js in the same folder
 // The add, mul and div modules from maths.js are being imported in as a const module named maths
-
-//const maths = require('./maths'); //alternative:
-const maths = { add, mul, div } = require('./maths')
 const request = require('request')
+const fs = require('fs')
+    //const maths = require('./maths'); //alternative:
+const maths = { add, mul, div } = require('./maths');
+
 request('http://www.example.com', (error, response, body) => {
     if (error) console.log('error:', error) // print error if error
     else console.log('body:', body) // print html for the site
 })
 
-const fs = require('fs') // call in file system (fs)
-    // define file path for sample file to be used in function below
-let sample = 'lesson-1/activity-a/sample.json';
-
+// import { readFile } from 'fs' // call in file system (fs)
+// define file path for sample file to be used in function below
 
 // notate the log showing when this index module has run
 console.log('index.js has run')
@@ -23,7 +22,3 @@ console.log('index.js has run')
 console.log(maths.add(30, 20))
 console.log(maths.mul(30, 20))
 console.log(maths.div(30, 7))
-
-
-// Not part of lesson
-fs.readFile(sample, 'utf8', (err, data) => { console.log(data) });
